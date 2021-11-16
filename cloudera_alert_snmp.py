b76c54fc80601f1963d430856fdad05a7da01e4c
Evgeniy Ginzburg <nad.oby@gmail.com>
2021-11-16 19:13:57 +0100

    Add filtering by service type and keyword in messages

--- cloudera_alert_snmp.py
+++ cloudera_alert_snmp.py
@@ -4,12 +4,13 @@
 Docs about obtaining Cloudera MIB file
 https://docs.cloudera.com/cloudera-manager/7.4.2/monitoring-and-diagnostics/topics/cm-alerts-snmp.html
 """
-import json
+from json import load
 from configparser import ConfigParser
 from dateutil.parser import isoparse
 from pathlib import Path
-# import sys
-from pysnmp.hlapi import sendNotification, SnmpEngine, CommunityData, UdpTransportTarget, ContextData, NotificationType, ObjectIdentity
+from re import match, search
+from pysnmp.hlapi import (sendNotification, SnmpEngine, CommunityData,
+UdpTransportTarget, ContextData, NotificationType, ObjectIdentity)
 from struct import pack
 
 
@@ -54,20 +55,25 @@ def iterate_alerts(json, severity):
     filtered = []
     for item in json:
         attributes = item['body']['alert']['attributes']
-        print(item['body']['alert']['source'])
+        # print(item['body']['alert']['source'])
         # print(item['body']['alert']['timestamp']['iso8601'])
         # print(attributes['PREVIOUS_HEALTH_SUMMARY'])
         # print(attributes['CURRENT_HEALTH_SUMMARY'])
-        print(attributes['SERVICE_TYPE'][0])
-        print(attributes['SERVICE'][0])
-        print(attributes['__uuid'][0])
-        print(attributes['HEALTH_TEST_RESULTS'][0]['content'])
+        # print(attributes['SERVICE_TYPE'][0])
+        # print(attributes['SERVICE'][0])
+        # print(attributes['__uuid'][0])
+        # print(attributes['HEALTH_TEST_RESULTS'][0]['content'])
         # print(attributes['EVENTCODE'])
-        print(attributes['SEVERITY'][0])
-        print(attributes['CATEGORY'][0])
+        # print(attributes['SEVERITY'][0])
+        # print(attributes['CATEGORY'][0])
+        # print(t_conf['service_bl'])
 
-        if attributes['PREVIOUS_HEALTH_SUMMARY'] != attributes['CURRENT_HEALTH_SUMMARY'] and attributes['SEVERITY'][0] == severity:
-            #print("Status changed", attributes['HEALTH_TEST_RESULTS'])
+        if (
+            attributes['PREVIOUS_HEALTH_SUMMARY'] != attributes['CURRENT_HEALTH_SUMMARY']
+            and attributes['SEVERITY'][0] == severity
+            and not match(t_conf['service_bl'], attributes['SERVICE_TYPE'][0])
+            and not search(t_conf['messages'], attributes['HEALTH_TEST_RESULTS'][0]['content'])
+        ):
             ts = isoparse(item['body']['alert']['timestamp']['iso8601'])
             snmp_time = pack('>HBBBBBB', ts.year, ts.month, ts.day, ts.hour, ts.minute, ts.second, 0)
             dic_res = {
@@ -85,33 +91,36 @@ def iterate_alerts(json, severity):
             except KeyError:
                 pass
             filtered.append(dic_res)
-
-        elif attributes['PREVIOUS_HEALTH_SUMMARY'] == attributes['CURRENT_HEALTH_SUMMARY'] or attributes['SEVERITY'][0] != severity:
+        elif (
+            attributes['PREVIOUS_HEALTH_SUMMARY'] == attributes['CURRENT_HEALTH_SUMMARY'] or
+            attributes['SEVERITY'][0] != severity
+        ):
             pass
-            #print("Status unchanged", attributes['HEALTH_TEST_RESULTS'][0]['content'])
     return(filtered)
 
 
 if __name__ == '__main__':
 
-    config = ConfigParser()
-    config.read('snmp_filtered.ini')
-    service_wl = config.get('filters', 'service_whitelist')
-    service_bl = config.get('filters', 'service_blacklist')
-    severity = config.get('filters', 'alert_severity')
     t_conf = {}
+    config = ConfigParser()
+    config.read('cloudera_alert_snmp.ini')
     t_conf['addr'] = config.get('trap', 'ipaddr')
     t_conf['port'] = config.get('trap', 'port')
     t_conf['community'] = config.get('trap', 'community')
+    t_conf['service_wl'] = config.get('filters', 'service_whitelist')
+    t_conf['service_bl'] = config.get('filters', 'service_blacklist')
+    t_conf['messages'] = config.get('filters', 'messages_blackist')
+    t_conf['severity'] = config.get('filters', 'alert_severity')
 
-    JSON = json.load(open("test.json"))
-    a = iterate_alerts(JSON, severity)
+    JSON = load(open("test.json"))
+    a = iterate_alerts(JSON, t_conf['severity'])
     send_trap(a, t_conf)
 
 # DONE Alerts should be enumerated or iterated trough
 # DONE Alerts should be filtered by SEVERITY, Which alerts are pass trough defined by a configuration
 # DONE CURRENT_HEALTH_SUMMARY should be different from PREVIOUS_HEALTH_SUMMARY
-# TODO Alerts should be filtered by service
+# DONE Alerts should be filtered by service
+# DONE Alerts should be filtered by keyword in message
 # TODO Suppressed alerts should be filtered out
 # TODO (Optional) Alerts should be filtered by CLUSTER
 # TODO (Optional) Add looping over multiple messages if there are present
